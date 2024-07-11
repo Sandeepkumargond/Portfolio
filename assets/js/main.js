f02b3c33982247e526ee9218ded5ca7047332c80
Sandeep Kumar Gond <sandeepkumargond198@gmail.com>
2024-07-11 11:22:47 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,10 +47,29 @@ const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2000,
-    delay: 200,
+    delay: 150,
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+ScrollReveal({
+    distance: "100px",
+    duration: 2000,
+    delay: 200
+  });
+
+  ScrollReveal().reveal(" .education", {
+    origin: "top"
+  });
+  ScrollReveal().reveal(" .internship", {
+    origin: "top"
+  });
+  ScrollReveal().reveal(".project-title" , {
+    origin: "top"
+  });
+  ScrollReveal().reveal(".projects", {
+    origin: "top"
+  });
